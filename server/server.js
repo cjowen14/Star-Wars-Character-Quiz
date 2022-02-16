57947ca91e8fd872fa91190131d787c742b2ae70
Cory Owen <cjowen14@outlook.com>
2022-02-15 17:12:41 -0700

    Another Deployment Attempt

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
-const{SERVER_PORT} = process.env;
+const port = process.env.PORT || process.env.SERVER_PORT;
 const app = express();
 const{roster, bio, questionsList, startQuiz ,nextQuestion, results, yourBio, deleteChar, changeName} = require('./controller.js');
 const {seed} = require('./seed.js');
 
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
 
 
 app.post('/seed', seed);
@@ -24,4 +30,4 @@ app.delete('/api/:name', deleteChar);
 app.put('/api/:name', changeName);
 
 
-app.listen(4040, () => console.log(`Server is running on port ${SERVER_PORT}`));
+app.listen(4040, () => console.log(`Server is running on port ${port}`));
